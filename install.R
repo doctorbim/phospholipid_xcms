5fbcfd3151e10fba05356eddcd50e7b4e46deebe
doctorbim <fosjun@gmail.com>
2023-06-22 00:29:21 -0400

    Update install.R

--- install.R
+++ install.R
@@ -1,4 +1,4 @@
 install.packages(c("readr", "tidyr", "ggplot2"))
 if (!require("BiocManager", quietly = TRUE))
     install.packages("BiocManager")
-BiocManager::install(c("faahKO", "pander", "xcms"))
+BiocManager::install(c("faahKO", "pander", "xcms", "pheatmap"))
